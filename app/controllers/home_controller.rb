ed1c8c98c519eeb9f319c42d7262f5cdedb311ab
Andre Neves <drakferion@gmail.com>
2012-01-20 02:19:31 +0100

    Home page.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  # GET /home
+  def index
+    @guests = Guest.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @guests }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Tertulia::Application.routes.draw do
   #   resources :products
   
   resources :guests
+  resources :home
 
   # Sample resource route with options:
   #   resources :products do
@@ -52,7 +53,7 @@ Tertulia::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'guests#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
