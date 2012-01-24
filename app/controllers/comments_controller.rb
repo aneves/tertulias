537d454518ac2a1e5fc8e47e944734df9840a215
Andre Neves <drakferion@gmail.com>
2012-01-24 01:30:48 +0100

    Comments: cannot edit, date is automatic.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,6 +25,7 @@ class CommentsController < ApplicationController
   # GET /comments/new.json
   def new
     @comment = Comment.new
+	@comment.date = DateTime.current
 
     respond_to do |format|
       format.html # new.html.erb
@@ -32,15 +33,11 @@ class CommentsController < ApplicationController
     end
   end
 
-  # GET /comments/1/edit
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
   # POST /comments
   # POST /comments.json
   def create
     @comment = Comment.new(params[:comment])
+	@comment.date = DateTime.current
 
     respond_to do |format|
       if @comment.save
@@ -48,22 +45,6 @@ class CommentsController < ApplicationController
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /comments/1
-  # PUT /comments/1.json
-  def update
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
