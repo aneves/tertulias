edc7f43e6c4fb0b16b60f0fac3563d6f0bb1ea92
Andre Neves <drakferion@gmail.com>
2013-09-24 20:04:02 +0100

    Removed duplicate notices. Removed useless files. Fixed bug.
    Fixed bug in views/guests/_list .

--- app/controllers/event_items_controller.rb
+++
@@ -1,9 +0,0 @@
-class EventItemsController < ApplicationController
-  before_filter :populate_event
-
-  private
-  def populate_event
-    @event = Event.find[:event_name]
-    redirect_to events_path if @event == nil
-  end
-end
