df82b1eeb4aa93beaa0646aa53c4976ae2b7aa05
Andre Neves <drakferion@gmail.com>
2013-10-08 02:08:18 +0100

    Pre-compilation of assets to publish to heroku. :( :( >:(

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Tertulias
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # "To speed up asset precompiles, it’s recommended that you tell Rails to only partially load your app."
+    config.assets.initialize_on_precompile = false
   end
 end
