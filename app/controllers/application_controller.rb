a53d6c2348ab8ab7dc1759142075c72c7a120cad
Andre Neves <drakferion@gmail.com>
2013-12-21 02:27:47 +0000

    Redirects to user list, after invite.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
+  def after_invite_path_for(resource)
+    return users_path if current_user.admin?
+    return root_path
+  end
 end
